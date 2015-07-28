e5cea67977659ebfe04a157af8c830bdcc457e34
naomiajacobs <naomiajacobs@gmail.com>
2015-07-28 15:37:44 -0400

    Made types and arrays pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  var type = typeof obj;
+
+  //
+  if (type === 'number' || type === 'boolean') {
+    return obj.toString();
+  }
+
+  else if (obj === null) {
+    return 'null';
+  }
+
+  else if (type === 'string') {
+    return '"' + obj + '"';
+  }
+
+  else if (Array.isArray(obj)) {
+    var stringified = '[';
+    for (var i = 0; i < obj.length; i++) {
+      stringified += stringifyJSON(obj[i]);
+      if (i < obj.length-1) {
+        stringified += ',';
+      }
+    }
+    stringified += ']';
+    return stringified;
+  }
+
 };
