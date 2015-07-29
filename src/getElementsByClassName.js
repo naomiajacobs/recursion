3711667c50d4260cb0b7ca01afe49fc20c91405c
naomiajacobs <naomiajacobs@gmail.com>
2015-07-29 15:44:06 -0400

    Made getElementsByClassName specs pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  
+  //makes array of elements with the class
+  var yesHasClass = [];
+
+  var hasClass = function(elem) {
+
+    //only run if the child is an element node
+    if (elem.nodeType === 1) {
+
+      //add element to list if it has the class
+      if (elem.classList.contains(className)) {
+        yesHasClass.push(elem);
+        alert(elem);
+      }
+
+      //if element has children, run hasClass on each of its children
+      if (elem.childNodes.length) {
+        for (var i = 0; i < elem.childNodes.length; i++) {
+          hasClass(elem.childNodes[i]);
+        }
+      }
+    }
+  };
+
+  //call on the document
+  hasClass(document.body);
+
+  //return the list of elements
+  return yesHasClass;
+
 };
