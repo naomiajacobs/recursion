464b6dde622971262fe5d036f3971ade53bce2c5
naomiajacobs <naomiajacobs@gmail.com>
2015-08-12 14:40:18 -0700

    Refactored to get rid of inner function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,34 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(className, elem) {
   
   //makes array of elements with the class
   var yesHasClass = [];
 
-  var hasClass = function(elem) {
+  elem = elem || document.body;
 
-    //only runs if the child is an element node
-    if (elem.nodeType === 1) {
+  //only runs if the child is an element node
+  if (elem.nodeType === 1) {
 
-      //add element to list if it has the class
-      if (elem.classList.contains(className)) {
-        yesHasClass.push(elem);
-      }
-
-      //if element has children, run hasClass on each of its children
-      if (elem.hasChildNodes()) {
-
-        _.each(elem.childNodes, function(el) {
-          
-          hasClass(el);
-        });
-      }
+    //add element to list if it has the class
+    if (elem.classList.contains(className)) {
+      yesHasClass.push(elem);
     }
-  };
 
-  //call on the document
-  hasClass(document.body);
+    //call hasClass on elem's children
+    _.each(elem.childNodes, function(el) {
+      
+      var childResults = getElementsByClassName(className, el);
+      yesHasClass = yesHasClass.concat(childResults);
+    });
+  }
 
   //return the list of elements
   return yesHasClass;
